03b5d927ffd6e3aa232cf86e88bcd22fd6145f97
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-17 08:39:14 +0300

    Cloudinary config added | /api/users route added

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import cookieParser from 'cookie-parser'
 
 // Database
 import connectDB from './models/connectMongoDB.js'
 
 // Routes
 import authRoutes from './routes/authRoutes.js'
-import cookieParser from 'cookie-parser'
+import userRoutes from './routes/userRoutes.js'
 
+import {v2 as cloudinary} from 'cloudinary'
 
 dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 const app = express()
 
 connectDB()
@@ -18,7 +25,10 @@ connectDB()
 app.use(cookieParser())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
+
+{/* Routes */}
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 
 
 
