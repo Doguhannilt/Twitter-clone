8b3d1ab191868e04189ba1ffe17ce218d3bc9744
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-17 09:58:30 +0300

    likedPosts added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -49,7 +49,14 @@ const userSchema = new mongoose.Schema({
     link: {
         type: String,
         default: ''
-    }
+    },
+    likedPosts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post',
+            default: []
+        }
+    ]
 },
     {
         timestamps: true
